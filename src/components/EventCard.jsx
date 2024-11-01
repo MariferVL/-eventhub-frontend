4bf7aaffd8f62d21d4aa1c6271737adade499bb1
MariferVL <mfvl.1186@gmail.com>
2024-10-31 23:59:15 -0300

    Create eventcard component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,12 +1,12 @@
-// const EventCard = ({ event }) => {
-//     return (
-//       <div className="border rounded-lg p-4">
-//         <h2 className="text-lg font-bold">{event.title}</h2>
-//         <p>{event.date}</p>
-//         <p>{event.description}</p>
-//       </div>
-//     );
-//   };
+const EventCard = ({ event }) => {
+    return (
+      <div className="border rounded-lg p-4">
+        <h2 className="text-lg font-bold">{event.title}</h2>
+        <p>{event.date}</p>
+        <p>{event.description}</p>
+      </div>
+    );
+  };
   
-//   export default EventCard;
+  export default EventCard;
   
