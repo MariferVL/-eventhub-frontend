8ed3963535b44376d9155c799f8e3fc6c803b47b
MariferVL <mfvl.1186@gmail.com>
2024-11-01 00:07:57 -0300

    Update proptypes in eventcard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,12 +1,22 @@
+import PropTypes from 'prop-types';
+
 const EventCard = ({ event }) => {
-    return (
-      <div className="border rounded-lg p-4">
-        <h2 className="text-lg font-bold">{event.title}</h2>
-        <p>{event.date}</p>
-        <p>{event.description}</p>
-      </div>
-    );
-  };
-  
-  export default EventCard;
-  
+  return (
+    <div className="border rounded-lg p-4">
+      <h2 className="text-lg font-bold">{event.title}</h2>
+      <p>{event.date}</p>
+      <p>{event.description}</p>
+    </div>
+  );
+};
+
+// Validación de PropTypes
+EventCard.propTypes = {
+  event: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired, // Asegura que event sea un objeto requerido con las propiedades especificadas
+};
+
+export default EventCard;
